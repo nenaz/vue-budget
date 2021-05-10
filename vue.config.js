9418f7b7bac85e5cfb761791d60afb9d96098f0e
mrnenaz <mrnenaz@gmail.com>
2021-05-10 22:28:54 +0300

    merge qr-scanner, add vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const fs = require('fs');
+const fs = require('fs');
 
 module.exports = {
   publicPath: process.env.VUE_APP_BASE_PATH,
@@ -11,11 +11,11 @@ module.exports = {
     },
   },
   devServer: {
-    // https: {
-    //   key: fs.readFileSync('/src/certsFiles/private.key', 'utf8'),
-    //   cert: fs.readFileSync('/src/certsFiles/cert.crt', 'utf8'),
-    // },
+    https: {
+      key: fs.readFileSync('./src/certsFiles/private.key', 'utf8'),
+      cert: fs.readFileSync('./src/certsFiles/cert.crt', 'utf8'),
+    },
     // https: true,
-    // public: 'https://localhost:8080/',
+    public: 'https://localhost:8080/',
   },
 };
